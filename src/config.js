39778f19fefe2f3ab732b160b3f88604050d1dfc
Nikodem Stamirski <nikoxx444@gmail.com>
2024-04-25 22:31:50 +0200

    added line selection by dragging for commenting

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default Object.freeze({
-    BACKEND_URL: "http://52.211.23.142"
+    BACKEND_URL: "http://127.0.0.1:8080"
 });
--- src/router.js
+++ src/router.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './views/HomePage.vue';
-import CommentPage from './views/CommentPage.vue';
 import ExploreSnippets from './views/ExploreSnippets.vue';
 import CodeSnippetPage from './views/CodeSnippetPage.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/explore', component: ExploreSnippets },
-  { path: '/comments', component: CommentPage, name: 'CommentPage' },
   { path: '/code_snippet/:code_snippet_id', component: CodeSnippetPage}
 ];
 
