7f670c7653112eb9bf865a563b4ec84620d56103
KamilTaras <kamiltaras0@gmail.com>
2024-05-09 23:49:44 +0200

    working notifications

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -12,9 +12,9 @@
           <span class="absolute -top-1 -right-1 block h-3 w-3 rounded-full text-center text-xs text-white bg-red-600" v-if="latestPosts.length > 0">{{ latestPosts.length }}</span>
         </button>
         <div v-if="showNotifications" class="absolute top-0 left-0 py-2 w-48 bg-white rounded-lg shadow-xl">
-          <template v-for="post in latestPosts" :key="post.CodeSnippetID">
-            <a :href="'/code_snippet/' + post.CodeSnippetID" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">
-              {{ post.Title }} <!-- Displaying the title of each code snippet -->
+          <template v-for="post in latestPosts" :key="post.id">
+            <a :href="'/code_snippet/' + post.id" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+              {{ post.text }} - {{ post.author }}
             </a>
           </template>
           
@@ -86,25 +86,36 @@ export default {
       }
     },
     fetchLatestPosts() {
-  let user_token = localStorage.getItem('user-token');
-  let user_id = localStorage.getItem('user_id');
-  axios.defaults.headers.common['Authorization'] = `Bearer ${user_token}`; // Set default header for all requests
-  axios.get(Config.BACKEND_URL + '/api/v1/user_code_snippet/' + user_id)
-    .then(response => {
-      // Log the full response data to see the structure
-      console.log('Full response:', response.data);
+    let userToken = localStorage.getItem('user-token');
+    let userId = localStorage.getItem('user_id');
+    
+    // Set up the authorization header
+    axios.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
 
-      // Assume the relevant data is in response.data.data
-      let codeSnippets = response.data.data.filter(snippet => snippet.CodeSnippetVersions.length > 0);
-      console.log('Filtered snippets:', codeSnippets);
-      
-      // Assign to latestPosts in the component's data
-      this.latestPosts = codeSnippets;
-    })
-    .catch(error => {
-      console.error('There was an error fetching the code snippets:', error);
-    });
-}
+    // Make the GET request to the notifications endpoint
+    axios
+      .get(`${Config.BACKEND_URL}/api/v1/notifications/${userId}`)
+      .then(response => {
+        // Log the full response to inspect the structure
+        console.log('Full response:', response.data);
+
+        // Extract notifications from the response
+        let notifications = Array.isArray(response.data) ? response.data : response.data.data || [];
+
+        // Map to notifications object suitable for your template
+        this.latestPosts = notifications.map(notification => ({
+          id: notification.NotificationID,
+          text: notification.Text,
+          author: notification.User && notification.User.username ? notification.User.username : 'Anonymous',
+        }));
+
+        // Log the final mapped notifications
+        console.log('Mapped notifications:', this.latestPosts);
+      })
+      .catch(error => {
+        console.error('Error fetching notifications:', error);
+      });
+  }
 
   },
   mounted() {
