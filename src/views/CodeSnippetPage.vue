b68d1475e6ef933e86e3d1a6dce1d705522d9380
Nikodem Stamirski <nikoxx444@gmail.com>
2024-05-09 19:30:21 +0200

    connected comments logic to server

--- src/views/CodeSnippetPage.vue
+++ src/views/CodeSnippetPage.vue
@@ -1,28 +1,31 @@
 <template>
     <div>
         <div v-if="codeSnippet">
-            <div v-for="codeSnippetVersion in codeSnippet.CodeSnippetVersions" :key="codeSnippetVersion.CodeSnippetVersionID">
-                <code-to-comment :Text = "codeSnippetVersion.Text" :CreatedAt = "codeSnippet.CreatedAt" :Username="codeSnippet.User ? codeSnippet.User.username : 'anonymous'" @lines = "getLines"></code-to-comment>
-                <div class="max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5" 
-                v-for="comment in comments"
-                :key="comment.id">
+            <div v-for="codeSnippetVersion in codeSnippet.CodeSnippetVersions"
+                :key="codeSnippetVersion.CodeSnippetVersionID">
+                <code-to-comment :Text="codeSnippetVersion.Text" :CreatedAt="codeSnippet.CreatedAt"
+                    :Username="codeSnippet.User ? codeSnippet.User.username : 'anonymous'"
+                    @lines="getLines"></code-to-comment>
+                <div class="max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5" v-for="comment in comments[codeSnippetVersion.CodeSnippetVersionID]"
+                    :key="comment.id">
                     <p class="text-gray-400 mb-4">@anonymous</p>
-                    <p><span class="text-xs font-semibold text-blue-100 bg-blue-600 px-2 py-1 rounded">Line: {{ comment.lines[0] }}-{{ comment.lines[comment.lines.length-1] }}</span></p>
+                    <p><span class="text-xs font-semibold text-blue-100 bg-blue-600 px-2 py-1 rounded">Line: {{
+            comment.lines[0] }}-{{ comment.lines[comment.lines.length - 1] }}</span></p>
                     <div class="w-full p-2 text-gray-300 bg-gray-800 rounded">
                         {{ comment.text }}
                     </div>
                     <div class="flext">
-                    <p class="text-gray-400 mt-3">{{ comment.date }}</p>
+                        <p class="text-gray-400 mt-3">{{ comment.date }}</p>
                     </div>
                 </div>
 
                 <div class="max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5">
                     <div class="flex flex-col space-y-4">
-                        <textarea class="w-full p-2 text-gray-300 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="Add a comment..."
-                                v-model="commentText"></textarea>
+                        <textarea
+                            class="w-full p-2 text-gray-300 bg-gray-800 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            placeholder="Add a comment..." v-model="commentText"></textarea>
                         <button class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded"
-                                @click="submitComment()">Submit Comment</button>
+                            @click="submitComment(codeSnippetVersion.CodeSnippetVersionID)">Submit Comment</button>
                     </div>
                 </div>
             </div>
@@ -46,7 +49,7 @@ export default {
             codeSnippet: null,
             commentText: "",
             lines: [1],
-            comments: [],
+            comments: {},
         };
     },
     created() {
@@ -59,33 +62,62 @@ export default {
         getLines(lines) {
             this.lines = lines;
         },
-        submitComment() {
-            if (this.commentText !== '') {
-                this.comments.push(
-                    {
-                        "id": Math.random() * (15 - 0) + 0,
-                        "text": this.commentText,
-                        "lines": this.lines,
-                        "date": new Date().toLocaleDateString(),
-                    }
-                );
-                this.commentText = "";
-            }
-        },
+        //         "codeSnippetVersionID": "string",
+        //   "commentID": "string",
+        //   "createdAt": "string",
+        //   "isGenerated": true,
+        //   "line": 0,
+        //   "replyComment": "string",
+        //   "replyCommentID": "string",
+        //   "text": "string",
+        //   "updatedAt": "string",
+        //  "userID": "string"
 
+
+        submitComment(codeSnippetVersionID) {
+            if (this.commentText !== '') {
+                axios.post(Config.BACKEND_URL + '/api/v1/review_comment/', {
+                    "codeSnippetVersionID": codeSnippetVersionID,
+                    "text": this.commentText,
+                    "line": this.lines[0],
+                    "isGenerated": false,
+                    "userID": localStorage.getItem('user-id'),
+                })
+                    .then(response => {
+                        console.log(response.data);
+                        this.fetchCodeSnippet()
+                    })
+                    .catch(error => {
+                        console.error('There was an error posting the comment:', error);
+                    });
+            }
+            this.commentText = "";
+        },
         fetchCodeSnippet() {
             console.log(this.$route.params.code_snippet_id);
-            axios.get(Config.BACKEND_URL+'/api/v1/code_snippet/' + this.$route.params.code_snippet_id)
+            axios.get(Config.BACKEND_URL + '/api/v1/code_snippet/' + this.$route.params.code_snippet_id)
                 .then(response => {
-                        console.log(response.data.data);
-                        this.codeSnippet = response.data.data;
-                    }
+                    console.log(response.data.data);
+                    this.codeSnippet = response.data.data;
+                    this.comments = response.data.data.CodeSnippetVersions.reduce((acc, version) => {
+                        var comments = version.ReviewComments.map(comment => {
+                            return {
+                                "id": comment.ReviewCommentID,
+                                "text": comment.Text,
+                                "lines": [comment.Line, comment.Line],
+                                "date": this.convertToReadableFormat(comment.CreatedAt),
+                            };
+                        });
+                        acc[version.CodeSnippetVersionID] = comments;
+                        return acc;
+                    }, {});
+                    console.log(this.comments);
+                }
                 )
                 .catch(error => {
                     console.error('There was an error fetching the code snippet:', error);
                 });
         },
-
         convertToReadableFormat(datetimeStr) {
             const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
             const date = new Date(datetimeStr);
@@ -95,5 +127,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
