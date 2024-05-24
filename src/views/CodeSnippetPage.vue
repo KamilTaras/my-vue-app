23e1fe5d5b039906db96141416c5091d1b1048d3
Nikodem Stamirski <nikoxx444@gmail.com>
2024-05-24 14:32:06 +0200

    added initial answering logic

--- src/views/CodeSnippetPage.vue
+++ src/views/CodeSnippetPage.vue
@@ -7,20 +7,35 @@
                     :Username="codeSnippet.User ? codeSnippet.User.username : 'anonymous'"
                     @lines="getLines"></code-to-comment>
                 <div v-for="comment in comments[codeSnippetVersion.CodeSnippetVersionID]" :key="comment.id"
-                :class="{'max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5': true, 'border-2 border-blue-500': answering === comment.id}"
-                >
+                    :class="{ 'max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5': true, 'border-2 border-blue-500': answering === comment.id }">
                     <p class="text-gray-400 mb-4">@{{ comment.username }}</p>
                     <p><span class="text-xs font-semibold text-blue-100 bg-blue-600 px-2 py-1 rounded">Line: {{
-            comment.lines[0] }}-{{ comment.lines[comment.lines.length - 1] }}</span></p>
+                            comment.lines[0] }}-{{ comment.lines[comment.lines.length - 1] }}</span></p>
                     <div class="w-full p-2 text-gray-300 bg-gray-800 rounded">
                         {{ comment.text }}
 
                     </div>
                     <div class="flext">
-                        
-                        <p class="text-gray-400 mt-3">{{ comment.date }}                        
-                            <button @click="startAnswering(comment.id)" class="text-xs text-blue-100 font-semibold bg-blue-600 hover:bg-blue-700 px-2 py-1 rounded float-right">Answer</button>
+
+                        <p class="text-gray-400 mt-3">{{ comment.date }}
+                            <button @click="startAnswering(comment.id)"
+                                class="text-xs text-blue-100 font-semibold bg-blue-600 hover:bg-blue-700 px-2 py-1 rounded float-right">Answer</button>
                         </p>
+                    </div>
+                    <div v-if="comment.replies.length > 0" class="ml-5">
+                        <div v-for="reply in comment.replies" :key="reply.id"
+                            class="max-w-2xl mx-auto bg-gray-700 rounded p-5 my-5">
+                            <p class="text-gray-400 mb-4">@{{ reply.username }}</p>
+                            <div class="w-full p-2 text-gray-300 bg-gray-800 rounded">
+                                {{ reply.text }}
+                            </div>
+                            <div class="flext">
+                                <p class="text-gray-400 mt-3">{{ reply.date }}
+                                    <button @click="startAnswering(reply.id)"
+                                        class="text-xs text-blue-100 font-semibold bg-blue-600 hover:bg-blue-700 px-2 py-1 rounded float-right">Answer</button>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -47,7 +62,7 @@
 import axios from 'axios';
 import Config from '../config.js';
 import CodeToComment from '../components/CodeToComment.vue';
-import { comment } from 'postcss';
+//import { comment } from 'postcss';
 export default {
     name: "CodeSnippetPage",
     data() {
@@ -119,15 +134,33 @@ export default {
                                 "date": this.convertToReadableFormat(comment.CreatedAt),
                                 "username": comment.User ? comment.User.username : "anonymous",
                                 "answering": comment.ReplyCommentID,
+                                "replies": [],
                             };
                         });
+                        // Create a map of comments for easy access
+                        var commentMap = comments.reduce((map, comment) => {
+                            map[comment.id] = comment;
+                            return map;
+                        }, {});
+
+                        // Assign reply comments to the comments they are answering to
+                        comments.forEach(comment => {
+                            if (comment.answering) {
+                                var parentComment = commentMap[comment.answering];
+                                if (parentComment) {
+                                    parentComment.replies.push(comment);
+                                }
+                            }
+                        });
+
+                        // Filter out reply comments from the top level
+                        comments = comments.filter(comment => !comment.answering);
+
                         acc[version.CodeSnippetVersionID] = comments;
                         return acc;
                     }, {});
-                    for (comment in this.comments[version.CodeSnippetVersionID]) {
-                        console.log(comment);
-                    }
-                        
+
+
                     console.log(this.comments);
                 }
                 )
